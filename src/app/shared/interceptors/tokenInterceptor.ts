5f12d8fa55ef1a394463955a6a0b88359f660c81
michele <vendramemichele94@gmail.com>
2020-01-19 21:22:37 +0100

    Added token interceptor

---
+++ src/app/shared/interceptors/tokenInterceptor.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+    constructor(private _authenticationService: AuthService) {}
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> { // Jwt token is added in authorization header
+        let currentUser = this._authenticationService.curentLoggedUserValue;
+        if (currentUser && currentUser.token) {
+            request = request.clone({
+                setHeaders: { 
+                    Authorization: `Bearer ${currentUser.token}`
+                }
+            });
+        }
+
+        return next.handle(request);
+    }
+}
