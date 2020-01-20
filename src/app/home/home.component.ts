bd915c88d94b0316c282089738eeab729efaf132
michele <vendramemichele94@gmail.com>
2020-01-20 19:33:03 +0100

    UI improvements

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { MoviesService } from '../shared/services/movies.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  // public images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  public images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   public movies: any[] = [];
 
   constructor(private _moviesService: MoviesService) { }
