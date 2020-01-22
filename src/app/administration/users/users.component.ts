9ca00ba3d1c388c87485cd1cef4c4dbf7c009396
michele <vendramemichele94@gmail.com>
2020-01-22 21:26:30 +0100

    Updated user form

--- src/app/administration/users/users.component.ts
+++ src/app/administration/users/users.component.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { IUser, IReservation } from 'src/app/shared/models/IUser';
 import { UsersService } from 'src/app/shared/services/users.service';
 import { ReservationService } from 'src/app/shared/services/reservation.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
@@ -17,7 +18,22 @@ export class UsersComponent implements OnInit {
   public selectedUser: IUser = null;
   public loading: boolean = false;
 
-  constructor(private _usersService: UsersService, private _reservationService: ReservationService) { }
+  //Selected user form
+  selectedUserForm: FormGroup;
+
+  constructor(
+    private _usersService: UsersService,
+    private _reservationService: ReservationService,
+    private _formBuilder: FormBuilder) {
+    this.selectedUserForm = this._formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      birthDate: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      fedelityCardNumber: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
     this._initUsers();
@@ -29,6 +45,14 @@ export class UsersComponent implements OnInit {
   }
 
   public selectUser(user: IUser) { //User was selected
+    this.selectedUserForm = this._formBuilder.group({
+      firstName: [user.firstName],
+      lastName: [user.lastName],
+      birthDate: [user.birthDate],
+      email: [user.email],
+      password: [user.password],
+      fedelityCardNumber: ['vuoto']
+    });
     this.selectedUser = user;
   }
 
@@ -45,10 +69,18 @@ export class UsersComponent implements OnInit {
     this._usersService.getUserById(id)
       .subscribe(user => {
         const indexOldUser = this.users.indexOf(this.selectedUser);
-        if(indexOldUser != -1) {
-          this.selectedUser = user;
-          this.users[indexOldUser] = this.selectedUser;
+        if (indexOldUser != -1) {
+          this.selectUser(user); //Update selecteduser + form
+          this.users[indexOldUser] = this.selectedUser; //Update user with changes locally in the users list to update UI
         }
       });
+  }
+
+  //Selected user form
+  public saveUserChanges() {
+
+  }
+  public resetSelectedUserForm() {
+    this.selectUser(this.selectedUser);
   }
 }
